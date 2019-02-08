33afd6f4bb52e769e64610a863f67db3a15a0f40
Jens Henneberg <hens.jenneberg@gmail.com>
2019-02-08 22:11:58 +0100

    Remove all unnecessary lines

--- app.py
+++ app.py
@@ -1,24 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
-from redis import Redis, RedisError
+
 import os
 import socket
-
-# Connect to Redis
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/")
 def hello():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
-
-    html = "Flask auf Python 2.7"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+    return "Flask auf Python 2.7"
 
 
 if __name__ == "__main__":
